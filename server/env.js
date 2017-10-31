01328696b1f09db90013e8a6ef102f3a3f064143
Ashutosh Kumar <looklikeme05@gmail.com>
2017-10-31 16:56:47 +0530

    Update env.js

--- server/env.js
+++ server/env.js
@@ -4,12 +4,12 @@ var path = require('path'),
 module.exports = {
 	development: {
 		rootPath: rootPath,
-		db: 'mongodb://testfwrk:testfwrk@ds147842.mlab.com:47842/fwrk',
+		db: 'mongodb://tddestfdddwrk:ddd@ds147822242.mlab.com:47822242/fwrk',
 		port: process.env.PORT || 8080
 	},
 	production: {
 		rootPath: rootPath,
-		db: 'mongodb://testfwrk:testfwrk@ds147842.mlab.com:47842/fwrk' || 'you can add a mongolab uri here ($ heroku config | grep MONGOLAB_URI)',
+		db: 'mongodb://testfwrk:teseeetfwrk@ds147822242.mlab.com:47222842/fw22rk' || 'you can add a mongolab uri here ($ heroku config | grep MONGOLAB_URI)',
 		port: process.env.PORT || 80
 	}
 };
